7faf38ce977cdb68cc9d873dc967e40d1a3c1600
Clément Lemariey <clemlemariey@gmail.com>
2019-08-19 15:34:42 +0200

    agencement page App.vue + drawer

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,6 @@
 <template>
   <v-app>
-    <v-app-bar clipped-right dense flat collapse-on-scroll app>
-      <v-spacer></v-spacer>
+    <v-app-bar class="d-none d-sm-block" clipped-right dense flat collapse-on-scroll app>
       <v-toolbar-title class="headline text-uppercase">
         <span>Vuetifyd</span>
         <span class="font-weight-light">MATERIAL DESIGN</span>
@@ -10,13 +9,17 @@
       <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
     </v-app-bar>
 
-    <v-content>
+    <v-content class="tes">
       <router-view></router-view>
     </v-content>
 
-    <v-navigation-drawer v-model="drawer" right app>
+    <v-navigation-drawer v-model="drawer" class="pa-2" temporary right app>
+      <div class="vfb-center">
+        <span>VUETIFYD</span>
+        <span class="font-weight-light">MATERIAL DESIGN</span>
+      </div>
       <v-list nav class="py-0">
-        <v-list-item v-for="item in items" :key="item.title" link :to="item.to">
+        <v-list-item v-for="item in items" :key="item.title" active-class="vfb-active" link :to="item.to">
           <v-list-item-icon>
             <v-icon>{{ item.icon }}</v-icon>
           </v-list-item-icon>
@@ -26,33 +29,20 @@
         </v-list-item>
       </v-list>
       <template v-slot:append>
-      <div class="pa-2">
-        <v-list-item link to="/">
-          <v-list-item-icon>
-            <v-icon>mdi-home</v-icon>
-          </v-list-item-icon>
-          <v-list-item-content>
-            <v-list-item-title>Home</v-list-item-title>
-          </v-list-item-content>
-        </v-list-item>
-          <v-btn block @click.stop="drawer = !drawer"><v-icon>mdi-close</v-icon></v-btn>
-        </div>
+        <v-btn block @click.stop="drawer = !drawer"><v-icon>mdi-close</v-icon></v-btn>
       </template>
     </v-navigation-drawer>
 
     <v-bottom-navigation v-model="bottomNav" grow>
-      <v-btn value="recent">
-        <span>Recent</span>
-        <v-icon>mdi-history</v-icon>
+      <v-btn value="home">
+        <v-icon>mdi-home</v-icon>
       </v-btn>
 
       <v-btn value="favorites">
-        <span>Favorites</span>
         <v-icon>mdi-heart</v-icon>
       </v-btn>
 
       <v-btn value="nearby">
-        <span>Nearby</span>
         <v-icon>mdi-map-marker</v-icon>
       </v-btn>
 
@@ -79,3 +69,12 @@ export default {
   }
 }
 </script>
+<style lang="scss" scoped>
+.vfb-center {
+  display: table;
+  margin: 0 auto;
+}
+.vfb-active::before {
+  background-color: #fff;
+}
+</style>
