f993545a217420df4e9216de9dd98757eac3c2c1
Clément Lemariey <clemlemariey@gmail.com>
2019-12-09 10:00:45 +0100

    suprr v-app

--- src/views/auth/Login.vue
+++ src/views/auth/Login.vue
@@ -1,24 +1,22 @@
 <template>
-  <v-app id="inspire">
-    <v-content>
-      <v-container fill-height>
-        <v-layout align-center>
-          <v-flex xs12 sm8 md4>
-            <!-- <img src="@/assets/logo_spendash.png" style="display:block; width:75%; margin:0 auto 30px;"/> -->
-            <v-form ref="form" v-model="valid" lazy-validation>
-              <v-text-field v-model="email" name="login" label="Email" :rules="emailRules" type="text"></v-text-field>
-              <v-text-field v-model="password" id="password" name="password" label="Mot de passe" :rules="passwordRules" type="password"></v-text-field>
-              <v-btn class="gradientBack" color="primary" :disabled="!valid" @click="submit" block>
-                Se connecter
-              </v-btn>
-            </v-form>
-            <v-btn to="/signup" block flat>S'inscrire</v-btn>
-          </v-flex>
+  <v-content>
+    <v-container fill-height>
+      <v-layout align-center>
+        <v-flex xs12 sm8 md4>
+          <!-- <img src="@/assets/logo_spendash.png" style="display:block; width:75%; margin:0 auto 30px;"/> -->
+          <v-form ref="form" v-model="valid" lazy-validation>
+            <v-text-field v-model="email" name="login" label="Email" :rules="emailRules" type="text"></v-text-field>
+            <v-text-field v-model="password" id="password" name="password" label="Mot de passe" :rules="passwordRules" type="password"></v-text-field>
+            <v-btn class="gradientBack" color="primary" :disabled="!valid" @click="submit" block>
+              Se connecter
+            </v-btn>
+          </v-form>
+          <v-btn to="/signup" block flat>S'inscrire</v-btn>
+        </v-flex>
 
-        </v-layout>
-      </v-container>
-    </v-content>
-  </v-app>
+      </v-layout>
+    </v-container>
+  </v-content>
 </template>
 
 <script>
